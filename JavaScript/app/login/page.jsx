0146dce68ef12b2660ee82e474d80070ce1eb22e
Paul Dimitris Kyriou <pdkyriou@gmail.com>
2023-09-19 20:18:34 +0100

    INITIAL COMMIT 190923

---
+++ JavaScript/app/login/page.jsx
@@ -0,0 +1,16 @@
+"use client"
+import React from 'react'
+import LoginComponent from './LoginComponent'
+
+
+export default function login() {
+  return (
+    <main className='main bg-slate-100'>
+ <div className="flex items-center justify-center h-screen">
+  <LoginComponent />
+</div>
+    </main>
+  )
+}
+
+
